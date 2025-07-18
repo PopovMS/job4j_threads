fcc690ff1abf93081f8dc437262ea52752aaa1f8
Misha <m.popov83@gmail.com>
2025-07-18 15:34:16 +0500

    fix checkstyle error

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,10 +3,14 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> {
+
+                }
         );
         Thread second = new Thread(
-                () -> {}
+                () -> {
+
+                }
         );
         System.out.println("first status: " + first.getState());
         System.out.println("second status: " + second.getState());
